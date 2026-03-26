6a3ae049fdb759688b4ed282995b57347f657147
SDYashi <deepakmarskole88@gmail.com>
2026-03-26 13:46:41 +0530

    ok

--- api_testing.py
+++ api_testing.py
@@ -48,8 +48,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # BASE_URL = "http://10.98.7.221:8000"
 # BASE_URL = "https://pdfserv.mpwin.co.in"
 BASE_URL = "http://testing.mpwin.co.in/pdfbills"
-# PDF_ENDPOINT = "/generate-sync"
-PDF_ENDPOINT = "/generate-sync-v2"
+PDF_ENDPOINT = "/generate-sync"
+# PDF_ENDPOINT = "/generate-sync-v2"
 API_URL = f"{BASE_URL}{PDF_ENDPOINT}"
 PAYLOAD_MODE = "raw_html"
 
